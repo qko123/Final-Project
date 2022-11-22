034d4af17c8586253176c3a2399f19af67937c78
Quinn Kozak <quinn.kozak@icloud.com>
2022-11-22 09:23:23 -0600

    Initial Commit

---
+++ Final Project/Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_ProjectApp.swift
+//  Final Project
+//
+//  Created by Quinn Kozak on 11/22/22.
+//
+
+import SwiftUI
+
+@main
+struct Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
